1a3a793279e88529327e660a8a0193dbed393393
parulshukla03 <parul_shukla@hotmail.com>
2017-01-22 09:49:18 -0800

    Add files via upload

---
+++ SumOftwoNos.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+        package acadgildassignment;
+        public class SumOftwoNos
+{
+   
+        public static void main(String[] args)
+        {
+                
+        int a = 9;
+        int b = 21;
+        int sum  = b -(-a);
+            System.out.println(sum);
+        
+        
+}
+        }
+        
+       
+
+       
+
